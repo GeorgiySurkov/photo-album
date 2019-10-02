f3afc50b7ca42e3e01c2f8ed9711494e98f2fc8e
GeorgiySurkov <georgiy.surkov@gmail.com>
2019-10-02 21:08:18 +0500

    config bug fix

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ class DevConfig:
     SECRET_KEY = 'you_will never_guess'
     SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(BASE_DIR, "app.db")}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = os.path.join(BASE_DIR, 'media/images')
+    UPLOAD_FOLDER = os.path.join(BASE_DIR, os.path.normpath('/media/'))
