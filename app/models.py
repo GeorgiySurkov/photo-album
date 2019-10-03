0cd88feabdd750ef626db3db8a9e19fa37b435c2
GeorgiySurkov <georgiy.surkov@gmail.com>
2019-10-03 22:02:06 +0500

    thumbnail model update

--- app/models.py
+++ app/models.py
@@ -6,16 +6,18 @@ from os.path import join as path_join, normpath
 class Image(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     file_name = db.Column(db.String(64), unique=True, index=True)
-    file_path = db.Column(db.String(96), unique=True, index=True)
+    file_path = db.Column(db.String(128), unique=True, index=True)
+    thumbnail_path = db.Column(db.String(128), unique=True, index=True)
 
-    def set_file_path(self):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         # TODO check for files with the same names
-
         # files = []
         # for dir_entry in os.scandir()
-        file_name = self.file_name
-        # file_extension = file_name.rsplit('.', 1)[-1]
-        self.file_path = path_join(app.config['UPLOAD_FOLDER'], normpath(f'images/{file_name}'))
+        file = self.file_name
+        f_name, f_extension = file.rsplit('.', 1)
+        self.file_path = path_join(app.config['UPLOAD_FOLDER'], normpath(f'images/{file}'))
+        self.thumbnail_path = path_join(app.config['UPLOAD_FOLDER'], normpath(f'thumbnails/{file}'))
 
     def __repr__(self):
         return f'<Image file_name="{self.file_name}">'
