f8a6ee3f4bf8960041cc504c1a5e33c9838e016d
GeorgiySurkov <georgiy.surkov@gmail.com>
2019-10-10 23:22:20 +0500

    typo fix

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 
 class DevConfig:
-    SECRET_KEY = 'you_will never_guess'
+    SECRET_KEY = 'you_will_never_guess'
     SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(BASE_DIR, "app.db")}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.path.join(BASE_DIR, os.path.normpath('media'))
