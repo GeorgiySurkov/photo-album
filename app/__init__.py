a4f028db48d3c45ee0b38c31bf919b4d12481517
GeorgiySurkov <georgiy.surkov@gmail.com>
2019-10-14 21:50:20 +0500

    creating dirs for photos if they don't exist

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,24 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from config import ProdConfig
+from config import ProdConfig, BASE_DIR
 from PIL import Image
 import warnings
+import os
+from os import path
 
 warnings.simplefilter('error', Image.DecompressionBombWarning)
 
 app = Flask(__name__)
 app.config.from_object(ProdConfig)
+
+if not path.exists(path.join(BASE_DIR, 'media')):
+    os.mkdir(path.join(path.join(BASE_DIR, 'media')))
+if not path.exists(path.join(BASE_DIR, 'media', 'images')):
+    os.mkdir(path.join(path.join(BASE_DIR, 'media', 'images')))
+if not path.exists(path.join(BASE_DIR, 'media', 'thumbnails')):
+    os.mkdir(path.join(path.join(BASE_DIR, 'media', 'thumbnails')))
+
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
